61505701f910dc63ad8fcba8b1513b7334ed6353
Romain SCHIPMAN <romain@MacBook-Air-de-Romain.local>
2024-08-29 11:36:01 +0200

    add input component

---
+++ src/lib/components/atoms/rosch-input/helpers/generate-input-size/generate-input-size.ts
@@ -0,0 +1,71 @@
+import { SpacingSize, TextSize, Theme } from "../../../../../theme/theme.interface";
+import { generateFontSize } from "../../../../../utils/generate-font-size/generate-font-size";
+
+/**
+ * Props for the `generateInputSize` function.
+ * 
+ * @interface GenerateInputSizeProps
+ * @property {Theme} theme - The theme object containing spacing and text size information.
+ * @property {SpacingSize} [size] - Optional size property to determine padding and spacing.
+ * @property {TextSize} [fontSize] - Optional font size property to determine the font size.
+*/
+interface GenerateInputSizeProps {
+    theme: Theme;
+    size?: SpacingSize;
+    fontSize?: TextSize;
+}
+
+/**
+ * Generates the CSS for input size based on the provided theme, size, and font size.
+ * 
+ * This function dynamically generates CSS properties for padding and font size
+ * based on the `size` and `fontSize` values passed in. It uses the `theme` object to
+ * retrieve spacing and text size values.
+ * 
+ * @param {GenerateInputSizeProps} props - The properties required to generate input size CSS.
+ * @param {Theme} props.theme - The theme object containing spacing and text size information.
+ * @param {SpacingSize} [props.size] - Optional size property to determine padding and spacing.
+ * @param {TextSize} [props.fontSize] - Optional font size property to determine the font size.
+ * 
+ * @returns {string} The generated CSS for the input size, or an empty string if neither `size` nor `fontSize` are provided.
+*/
+const generateInputSize = ({ theme, fontSize, size } : GenerateInputSizeProps) => {
+  if(!fontSize || !size) {
+    if(!size && fontSize) {
+      return `font-size: ${generateFontSize(fontSize, theme.textSizes)};`;
+    }
+    return "";
+  }
+
+  let cssProperties = `
+        padding-left: ${generateFontSize(size, theme.spacings)};
+        padding-right: ${generateFontSize(size, theme.spacings)};
+        font-size: ${generateFontSize(fontSize, theme.textSizes)};
+  `;
+
+  if(size === "xs" || size === "sm") {
+    return `
+        ${cssProperties}
+        padding-top: ${generateFontSize("sm", theme.spacings)};
+        padding-bottom: ${generateFontSize("sm", theme.spacings)};
+    `;
+  }
+  if(size === "md" || size === "lg") {
+    return `
+        ${cssProperties}
+        padding-top: ${generateFontSize("md", theme.spacings)};
+        padding-bottom: ${generateFontSize("md", theme.spacings)};
+    `;
+  }
+
+  if(size === "xl" || size === "xxl") {
+    return `
+        ${cssProperties}
+        padding-top: ${generateFontSize("lg", theme.spacings)};
+        padding-bottom: ${generateFontSize("lg", theme.spacings)};
+    `;
+  }
+
+};
+
+export { generateInputSize };
---
+++ src/stories/rosch/atoms/rosch-input/rosch-input.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { RoschInput } from "../../../../lib/components/atoms/rosch-input/rosch-input";
+
+export default {
+  title: "Rosch/Atoms/Rosch-Input",
+  component: RoschInput,
+} as Meta<typeof RoschInput>;
+
+type Story = StoryObj<typeof RoschInput>;
+
+export const DefaultInput: Story = {
+  args: {
+    label: "username",
+    placeholder: "John Doe",
+    maxWidth: "20rem"
+  },
+};
+
+export const InputPassword: Story = {
+  args: {
+    label: "password",
+    type: "password",
+    maxWidth: "20rem"
+  },
+};
