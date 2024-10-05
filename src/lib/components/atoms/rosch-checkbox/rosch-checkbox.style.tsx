5ae095af95272741e868a95e80d60bff0f7d6b79
Romain SCHIPMAN <romain@MacBook-Air-de-Romain.local>
2024-10-05 18:19:04 +0200

    add style for checkbox

--- src/lib/components/atoms/rosch-checkbox/rosch-checkbox.style.tsx
+++ src/lib/components/atoms/rosch-checkbox/rosch-checkbox.style.tsx
@@ -1,16 +1,31 @@
 import styled from "styled-components";
 import { CssProperties } from "../../../interfaces/css-properties.interface";
 import { generateRadius } from "../../../utils/generate-radius/generate-radius";
+import { SpacingSize } from "../../../theme/theme.interface";
+import { generateSpacing } from "../../../utils/generate-spacing/generate-spacing";
 
 export interface RoschCheckboxStyledProps extends CssProperties{
-
+    size?: SpacingSize;
 }
 
 const RoschCheckboxStyled = styled.div<RoschCheckboxStyledProps>`
     gap: 0.5rem;
     display: flex;
     .checkbox__input {
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        -ms-appearance: none;
+    }
+    .checkbox__input {
+        width: ${({ size }) => generateSpacing(size ?? "md")};
+        height: ${({ size }) => generateSpacing(size ?? "md")};
+        background-color: transparent;
+        border: 1px solid black;
         border-radius: ${({ radius }) => generateRadius(radius ?? "none")};
+        
+        &:checked {
+            background-color: ${({ theme }) => theme.themeColors?.main?.primary?.highest};
+        }
     }
 `;
 
