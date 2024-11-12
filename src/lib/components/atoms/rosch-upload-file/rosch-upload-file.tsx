2c0fbbacf56cf3ffa5539915d11c1cb92165e3d2
Romain SCHIPMAN <romain@MacBook-Air-de-Romain.local>
2024-11-12 23:11:05 +0100

    change id for upload file

--- src/lib/components/atoms/rosch-upload-file/rosch-upload-file.tsx
+++ src/lib/components/atoms/rosch-upload-file/rosch-upload-file.tsx
@@ -3,12 +3,13 @@ import { RoschUploadFileStyled } from "./rosch-upload-file.style";
 import { RoschText } from "../rosch-text/rosch-text";
 
 export interface RoschUploadFileProps {
+    id: string;
     className?: string;
     urlImg?: string;
     onFileChange?: (file: File) => void;
 }
 
-const RoschUploadFile : FunctionComponent<RoschUploadFileProps> = ({ className, onFileChange, urlImg }) => {
+const RoschUploadFile : FunctionComponent<RoschUploadFileProps> = ({ id, className, onFileChange, urlImg }) => {
     const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
         const selectedFile = event.target.files?.[0];
         if (selectedFile && selectedFile.type.startsWith("image/") && onFileChange) {
@@ -16,13 +17,11 @@ const RoschUploadFile : FunctionComponent<RoschUploadFileProps> = ({ className, 
         }
     };
 
-    //let url = "http://localhost:8000/uploads/1728149149184-collection-diamond.png";
-
     return (
         <RoschUploadFileStyled className={className}>
-            <label htmlFor="file-upload" className="upload__box">
+            <label htmlFor={`upload-file-${id}`} className="upload__box">
                 <input
-                    id="file-upload"
+                    id={`upload-file-${id}`}
                     type="file"
                     accept="image/*"
                     style={{display: "none"}}
@@ -31,7 +30,7 @@ const RoschUploadFile : FunctionComponent<RoschUploadFileProps> = ({ className, 
                 {
                     !urlImg ?
                         <div className="upload__box__content">
-                            <RoschText id={"dz"} fontSize="xs">Cliquez ici pour uploader un fichier</RoschText>
+                            <RoschText id={`upload-file-text-${id}`} fontSize="xs">Cliquez ici pour uploader un fichier</RoschText>
                         </div>
                         :
                         <img src={urlImg} className="upload__box_image" alt=""/>
